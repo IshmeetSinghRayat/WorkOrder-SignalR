5b37218fe3830a75887f6ae4b01d58de28349792
ishmeet.rayat <developer.ishmeet@adek.gov.ae>
2021-02-14 10:16:52 +0400

    connit for versioon change of project

---
+++ WorkOrderApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkOrderApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
